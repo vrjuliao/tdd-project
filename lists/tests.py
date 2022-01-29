cca3e4de522d9d77e2edf9e5f07b2b636e1fbe46
vrjuliao <vrjuliao@hotmail.com>
2022-01-28 21:51:49 -0300

    Apresentar Múltiplos Itens na Listagem

--- lists/tests.py
+++ lists/tests.py
@@ -24,6 +24,15 @@ class HomePageTest(TestCase):
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response['location'], '/')
 
+    def test_displays_all_list_items(self):
+        Item.objects.create(text='itemey 1')
+        Item.objects.create(text='itemey 2')
+
+        response = self.client.get('/')
+
+        self.assertIn('itemey 1', response.content.decode())
+        self.assertIn('itemey 2', response.content.decode())
+
 class ItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
