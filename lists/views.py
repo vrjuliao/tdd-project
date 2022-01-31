3b29e7bae2cfb95b9ff74c118d6ec424227986d8
vrjuliao <vrjuliao@hotmail.com>
2022-01-31 12:38:06 -0300

    Novo Projeto: Atualizando o Template

--- lists/views.py
+++ lists/views.py
@@ -4,12 +4,9 @@ from lists.models import Item, List
 def home_page(request):
     return render(request, 'home.html')
 
-    return render(request, 'home.html')
-
 def view_list(request, list_id):
     my_list = List.objects.get(id=list_id)
-    items = Item.objects.filter(list=my_list)
-    return render(request, 'list.html', {'items': items})
+    return render(request, 'list.html', {'list': my_list})
 
 def new_list(request):
     my_list = List.objects.create()
